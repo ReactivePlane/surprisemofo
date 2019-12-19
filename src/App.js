cae431b12a46d68b0e6b3998edd6dd842b1f3435
unknown <gnamu14@freeuni.edu.ge>
2019-12-19 20:04:24 +0400

    here we go

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Surprise Motherfucker!
+          Surprise Motherfucker!GGWP
         </p>
         <a
           className="App-link"
