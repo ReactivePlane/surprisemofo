8a6f4cfa3029099f9e4d32a1e073a44e4d850cf1
unknown <gnamu14@freeuni.edu.ge>
2019-12-19 21:27:03 +0400

    nn

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Surprise Motherfucker!GGWP
+          Surprise Motherfucker! nemsky
         </p>
         <a
           className="App-link"
