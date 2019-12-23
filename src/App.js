e3300d985569e6f3b291734180a4075850d86317
Reacter <gnamu14@freeuni.edu.ge>
2019-12-23 18:19:09 +0400

    nemo, new files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Registration from './Routes/Registration/Registration'
 import {Route,Switch} from 'react-router-dom'
 import Home from './Routes/Home/Home'
 
